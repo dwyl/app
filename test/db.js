18914e90905ca6e40783b0d6af70f710ddce7bf4
nelsonic <nodecoder@gmail.com>
2014-07-11 22:03:48 +0100

    adds update record mock

--- test/db.js
+++ test/db.js
@@ -8,16 +8,32 @@ var db = new couchbase.Connection();
 var Lab = require("lab");
 Lab.experiment("CouchDB Tests", function() {
   // tests
-  Lab.test("Set & Get a Value", function(done) {
+  Lab.test("Set & Get a record", function(done) {
     db.set('testdoc', {name:'test'}, function(err, result) {
-      // if (err) throw err;
 
       db.get('testdoc', function(err, result) {
-        // if (err) throw err;
+
         Lab.expect(result.value.name).to.equal('test');
         // console.log(result.value);
-        done(); // done() callback is required to end the test.
+        done();
       });
     });
   });
+
+  Lab.test("Update a record", function(done) {
+    db.set('testdoc', {name:'initial'}, function(err, result) {
+      // if (err) throw err;
+      db.set('testdoc', {name: 'updated'}, function(err, result) {
+        db.get('testdoc', function(err, result) {
+          // if (err) throw err;
+          Lab.expect(result.value.name).to.equal('updated');
+          // console.log(result.value);
+          done(); // done() callback is required to end the test.
+        });
+      });
+    });
+  });
+
+  
+
 });
