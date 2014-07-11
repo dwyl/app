2b72e912dae09eef47900fe54c6e1d4780e6729a
nelsonic <nodecoder@gmail.com>
2014-07-11 21:36:29 +0100

    adds set/get tests (mock) couchbase

--- test/db.js
+++ test/db.js
@@ -1,16 +1,23 @@
-var couchbase = require('couchbase');
-var cluster = new couchbase.Cluster();
-var db = cluster.openBucket('default');
+// var couchbase = require('couchbase');
+// var cluster = new couchbase.Cluster();
+// var db = cluster.openBucket('default');
 
-db.set('testdoc', {name:'Frank'}, function(err, result) {
-  if (err) throw err;
+var couchbase = require('couchbase').Mock;
+var db = new couchbase.Connection();
 
-  db.get('testdoc', function(err, result) {
-    if (err) throw err;
+var Lab = require("lab");
+Lab.experiment("CouchDB Tests", function() {
+  // tests
+  Lab.test("Set & Get a Value", function(done) {
+    db.set('testdoc', {name:'test'}, function(err, result) {
+      // if (err) throw err;
 
-    console.log(result.value);
-    // {name: Frank}
+      db.get('testdoc', function(err, result) {
+        // if (err) throw err;
+        Lab.expect(result.value.name).to.equal('test');
+        // console.log(result.value);
+        done(); // done() callback is required to end the test.
+      });
+    });
   });
 });
-
-console.log('Couchbase says hai!');
