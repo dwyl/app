781e699e929ab9f3da690a1b100ce05b020d26eb
nelsonic <contact.nelsonic@gmail.com>
2015-02-05 12:45:36 +0000

    update lab to latest verison. update tests. dont like it... https://github.com/nelsonic/time/issues/8

--- test/db.js
+++ test/db.js
@@ -1,31 +1,67 @@
-var couchbase = require('couchbase').Mock;
-var db = new couchbase.Connection();
-var d = require('../lib/time');
-
-// console.log(d);
-
+var ES = require('esta');
 var Lab = require("lab");
-Lab.experiment("CouchDB Tests", function() {
+var lab = exports.lab = Lab.script();
+var expect = require('code').expect;
+var suite = lab.suite;
+var test = lab.test;
+
+var record =  {
+  index: "time",
+  type: "timer",
+  id: Math.floor(Math.random() * (1000000)),
+  start: new Date().getTime()
+}
+var rec = {}; // make a copy of rec for later.
+for(var key in record) {
+  if(record.hasOwnProperty(key)) {
+    rec[key] = record[key];
+  }
+}
+
+suite("ES CRUD Tests", function() {
   // tests
-  Lab.test("Set & Get a record", function(done) {
-    db.set('testdoc', {name:'test'}, function(err, result) {
+  test("CONNECT TO ES", function(done) {
+    ES.CONNECT(function(err, result) {
+      // console.log('STATUS: '+result.status);
+      expect(parseInt(result.status,10)).to.equal(200);
+      done();
+    });
 
-      db.get('testdoc', function(err, result) {
+  });
 
-        Lab.expect(result.value.name).to.equal('test');
-        // console.log(result.value);
+  test("CREATE & READ a record", function(done) {
+    ES.CREATE(record, function(result) {
+
+      ES.READ(rec, function(result) {
+        // console.log(result);
+        expect(result._source.start).to.equal(rec.start);
         done();
       });
     });
   });
 
-  Lab.test("Update a record", function(done) {
-    db.set('testdoc', {name:'initial'}, function(err, result) {
+  test("UPDATE a record", function(done) {
+    var record =  {
+      index: "time",
+      type: "timer",
+      id: Math.floor(Math.random() * (1000000)),
+      start: new Date().getTime()
+    }
+    var rec = {}; // make a copy of rec for later.
+    for(var key in record) {
+      if(record.hasOwnProperty(key)) {
+        rec[key] = record[key];
+      }
+    }
+    ES.CREATE(record, function(res) {
       // if (err) throw err;
-      db.set('testdoc', {name: 'updated'}, function(err, result) {
-        db.get('testdoc', function(err, result) {
-          // if (err) throw err;
-          Lab.expect(result.value.name).to.equal('updated');
+      rec.end = new Date().getTime();
+      ES.UPDATE(rec, function(res) {
+        // console.log(rec);
+        ES.READ(rec, function(res) {
+          // console.log(res);
+          expect(res._source.end).to.equal(rec.end);
+          expect(res._version).to.equal(2);
           // console.log(result.value);
           done(); // done() callback is required to end the test.
         });
@@ -33,4 +69,8 @@ Lab.experiment("CouchDB Tests", function() {
     });
   });
 
+});
+
+process.on('uncaughtException', function(err) {
+  // console.log('Something is not right there ... ' + err);
 });
