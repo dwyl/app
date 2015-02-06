dc299a6c1376aa75e4bc7b7247258966675c347a
nelsonic <contact.nelsonic@gmail.com>
2015-02-06 17:35:29 +0000

    changes test/db.js to use tape instead of lab - https://github.com/nelsonic/time/issues/10

--- test/test.js
+++ test/test.js
@@ -1,26 +1,26 @@
 var Lab = require("lab");
-var lab = exports.lab = Lab.script();
-var expect = require('code').expect;
-var suite = lab.suite;
-var test = lab.test;
+var lab = exports.lab = require('lab').script();
+var assert = require('assert');
+// var suite = lab.suite;
+// var test = lab.test;
 var server = require("../"); // require index.js
 
-suite("Basic HTTP Tests", function() {
+lab.suite("Basic HTTP Tests", function() {
     // tests
-  test("GET timer /timer/{id?} should fail (at first)", function(done) {
+  lab.test("GET timer /timer/{id?} should fail (at first)", function(done) {
     var options = {
       method: "GET",
       url: "/timer/1"
     };
         // server.inject lets you similate an http request
     server.inject(options, function(response) {
-      expect(response.statusCode).to.equal(404);  //  Expect http response status code to be 200 ("Ok")
+      assert.equal(response.statusCode, 404);  //  Expect http response status code to be 200 ("Ok")
    // Lab.expect(response.result).to.have.length(12); // Expect result to be "Hello Timmy!" (12 chars long)
       done();                                         // done() callback is required to end the test.
     });
   });
 
-  test("POST timer /timer should create a new timer", function(done) {
+  lab.test("POST timer /timer should create a new timer", function(done) {
     var options = {
       method: "POST",
       url: "/timer",
@@ -28,12 +28,10 @@ suite("Basic HTTP Tests", function() {
     };
         // server.inject lets you similate an http request
     server.inject(options, function(response) {
-      expect(response.statusCode).to.equal(400);  //  Expect http response status code to be 200 ("Ok")
+      assert.equal(response.statusCode, 400);  //  Expect http response status code to be 200 ("Ok")
    // Lab.expect(response.result).to.have.length(12); // Expect result to be "Hello Timmy!" (12 chars long)
       done();                                         // done() callback is required to end the test.
     });
   });
-
-
 
 });
