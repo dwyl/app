a765d06f623ca6849fd205d76075180cddce9490
nelsonic <contact.nelsonic@gmail.com>
2015-02-18 21:49:17 +0000

    update models to use Joi validation - https://github.com/ideaq/time/issues/17

---
+++ models/timer.js
@@ -0,0 +1,11 @@
+var Joi = require('joi');
+module.exports = {
+  payload: {
+    person: Joi.string(),
+    desc: Joi.string().optional(),
+    ct: Joi.forbidden(), // don't allow people to set this!
+    st: Joi.date().iso(),
+    et: Joi.date().iso().optional(),
+    aid: Joi.string()
+  }
+}
