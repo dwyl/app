9dca4096f2fb4c3357676f7ebc87a29eec4b2859
nelsonic <contact.nelsonic@gmail.com>
2015-03-15 17:04:15 +0000

    adds lout to api/server.js to provide api docs. https://github.com/nelsonic/learn-hapi/issues/6

--- api/server.js
+++ api/server.js
@@ -1,24 +1,26 @@
+// if you want to run the Just the API server without front-end run this file
 var Hapi    = require('hapi');
 var Basic   = require('hapi-auth-basic');
 var AuthJWT = require('hapi-auth-jwt2')
 var Joi     = require('joi');
+var lout    = require('lout');
 var ES      = require('esta');  // https://github.com/nelsonic/esta
 var port    = process.env.PORT || 1337; // heroku define port or use 1337
 var server  = new Hapi.Server();
 
 server.connection({ port: port });
 
-var routes = require('./api/routes.js');
+var routes = require('./routes.js');
 
-server.register([ {register: Basic}, {register: AuthJWT} ], function (err) {
+server.register([ {register: Basic}, {register: AuthJWT}, { register: lout } ], function (err) {
 
   server.auth.strategy('basic', 'basic', {
-    validateFunc: require('./api/lib/auth_basic_validate.js')
+    validateFunc: require('./lib/auth_basic_validate.js')
   });
 
   server.auth.strategy('jwt', 'jwt', 'required',  {
     key: process.env.JWT_SECRET,
-    validateFunc: require('./api/lib/auth_jwt_validate.js')
+    validateFunc: require('./lib/auth_jwt_validate.js')
   });
 
   server.route(routes);
