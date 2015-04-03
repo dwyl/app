919a5a734502eb334a17518f70d12312644fffd0
nelsonic <contact.nelsonic@gmail.com>
2015-04-03 14:37:29 +0100

    remove underscore prefixed fields (returned by ElasticSearch) https://github.com/ideaq/time/issues/90

--- api/handlers/timer_start.js
+++ api/handlers/timer_start.js
@@ -10,21 +10,28 @@ module.exports = function(req, reply) {
     type: "timer",
     person: decoded.person,
     session: decoded.jti, // session id from JWT
-    created: new Date().toISOString(),
+    ct: new Date().toISOString(),
     id: aguid()
   }
 
   for (var k in req.payload){
     timer[k] = req.payload[k]; // extract values from payload
   }
-  if(!timer.start) { // client did not define the start time
-    timer.start = timer.created; // set it to the same as created
+  if(!req.payload.start) { // client did not define the start time
+    timer.start = timer.ct; // set it to the same as created
   } else {
     // allow the client to set the started time
   }
 
   ES.CREATE(timer, function(record) {
     Hoek.merge(record, timer); // http://git.io/Amv6
+    // delete the useless _prefixed fields returned by ES
+    record.id = record._id;
+    delete record._index;
+    delete record._type;
+    delete record._version;
+    delete record.person;
+    delete record._id
     reply(record);
   })
 }
