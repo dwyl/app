fd726cab33957543e3730583d04586d5eb24eef7
nelsonic <contact.nelsonic@gmail.com>
2015-04-09 17:29:57 +0100

    seamless registration for anonymous person who has created several timers. https://github.com/ideaq/time/issues/102

--- api/lib/transfer_anon_to_registered.js
+++ api/lib/transfer_anon_to_registered.js
@@ -12,10 +12,7 @@ var JWT  = require('jsonwebtoken'); // https://github.com/docdis/learn-json-web-
 
 module.exports = function(req, reply, personid) {
   JWT.verify(req.headers.authorization, process.env.JWT_SECRET, function(err, decoded) {
-    console.log(" - - - - Transfer - - - - ")
-    // console.log(req.auth.credentials);
-    console.log(personid);
-    // console.log(decoded);
+
     var session = {
       index : "time",
       type  : "session",
@@ -48,78 +45,24 @@ module.exports = function(req, reply, personid) {
             "text": decoded.jti
           };
 
-          // console.log(" - - - - - - - - - - SEARCH QUERY: ");
-          // console.log(query)
-          // console.log(" - - - - - - - - - - ");
           ES.SEARCH(query, function(res) {
-            console.log(res.hits.total);
-            console.log(" - - - - - - - - - - hits: ");
-            console.log(res.hits.hits[0]);
-            if(res.hits.total > 0) {
-              return reply(res);
-            }
-            else {
-
-            }
+            // console.log(res.hits.total);
+            // console.log(" - - - - - - - - - - hits: ");
+            res.hits.hits.forEach(function(hit){
+              // console.log(hit);
+              var timer = hit._source;
+              timer.id = hit._id;
+              timer.index = hit._index;
+              timer.type = hit._type;
+              timer.person = personid; // the whole point of this!
+              ES.UPDATE(timer, function(res){
+                // console.log("VERSION:",res._version);
+              })
+            })
+            return reply(res);
           }); // END SEARCH
         })
       }); // END session UPDATE
-
     });
-
   });
-  // var decoded = req.auth.credentials;
-
-  // var timer =  {
-  //   index:   "time",
-  //   type:    "timer",
-  //   id: req.payload.id
-  // }
-  // // FIRST lookup the timer to see if it exists
-  // ES.READ(timer, function(esres) {
-  //   // console.log(esres);
-  //   // console.log(" - - - - - - - - - - - - ");
-  //   if(esres.found){
-  //     for (var k in esres._source) {
-  //       timer[k] = esres._source[k]; // extract values from existing record
-  //     }
-  //     // UPDATE the relevant fields:
-  //     for (var j in req.payload) {
-  //       timer[j] = req.payload[j]; // extract values from payload
-  //     }
-  //   }
-  //   else { // create a NEW Timer Record:
-  //     timer.person  = decoded.person;
-  //     timer.session = decoded.jti; // session id from JWT
-  //     timer.ct      = new Date().toISOString();
-  //     //a timer should ALWAYS have a start time
-  //     if(!req.payload.start) { // client did not define the start time
-  //       timer.start = timer.ct; // set it to the same as created
-  //     } else {
-  //       // allow the client to set (and UPDATE!) the started time
-  //     }
-  //     // UPDATE the relevant fields:
-  //     for (var l in req.payload) {
-  //       timer[l] = req.payload[l]; // extract values from payload
-  //     }
-  //     // BUT don't allow setting id for *NEW* Timers
-  //     timer.id = aguid();
-  //   }
-  //
-  //
-  //   ES.UPDATE(timer, function(record) {
-  //     Hoek.merge(record, timer); // http://git.io/Amv6
-  //     record.id = record._id;
-  //     delete record._index;
-  //     delete record._type;
-  //     delete record._version;
-  //     delete record.person;
-  //     delete record._id
-  //     reply(record);
-  //   })
-  // });
-  // // delete the excess fields so we don't bloat the record:
-  // delete req.payload._index;
-  // delete req.payload._type;
-  // delete req.payload._version;
 }
--- api/test/transfer_anon_to_registered.js
+++ api/test/transfer_anon_to_registered.js
@@ -2,7 +2,9 @@ var test   = require('tape');
 var server = require("../../web.js");
 var dir    = __dirname.split('/')[__dirname.split('/').length-1];
 var file   = dir + __filename.replace(__dirname, '') + " -> ";
+var email  = "n"+Math.random()+"@awesome.net"
 var token;
+var tid;
 test(file + "Anonymous person can creates a timer, then register", function(t) {
   var options    = {
     method  : "GET",
@@ -23,9 +25,10 @@ test(file + "Anonymous person can creates a timer, then register", function(t) {
     };
     server.inject(options, function(res) {
       var T = JSON.parse(res.payload);
-      console.log(file+" - - - - anonymous person's timer:")
-      console.log(T);
-      console.log(" - - - - - - - - - - - - - - - - - - - ")
+      // console.log(file+" - - - - anonymous person's timer:")
+      // console.log(T.id);
+      tid = T.id; // save this to lookup below
+      // console.log(" - - - - - - - - - - - - - - - - - - - ")
       t.equal(res.statusCode, 200, "New timer started! " + T.start);
       t.equal(T.desc, timer.desc, "New timer desc: " + timer.desc);
       t.end();
@@ -35,9 +38,10 @@ test(file + "Anonymous person can creates a timer, then register", function(t) {
 
 test(file + "Register anon", function(t){
   var person = {
-    "email"    : Math.random()+"@awesome.net",
+    "email"    : email,
     "password" : "SoCloseTo10kh!"
   }
+
   var options = {
     method  : "POST",
     url     : "/register",
@@ -46,8 +50,29 @@ test(file + "Register anon", function(t){
   };
   setTimeout(function(){
     server.inject(options, function(res) {
-      console.log(res.result);
+      // console.log(res.result);
       t.equal(res.statusCode, 200, "Person registration is succesful");
+      t.end();
+    });
+  },1000);
+})
+
+var aguid = require('aguid');
+
+test(file + "Lookup the timer confirm the person is set", function(t){
+  var personid = aguid(email);
+
+  var options = {
+    method  : "GET",
+    url     : "/timer/"+tid,
+    headers : { authorization : token }
+  };
+  // console.log("URL: "+options.url);
+  setTimeout(function(){
+    server.inject(options, function(res) {
+      // console.log(" - - -  person should not be anonymous anymore")
+      // console.log(res.result);
+      t.equal(res.result.person, personid, "Timer");
       t.end();
       server.stop();
     });
@@ -55,9 +80,9 @@ test(file + "Register anon", function(t){
 })
 
 var drop = require('./z_drop');
-test("Teardown", function(t) {
-  drop(function(res){
-    t.equal(res.acknowledged, true, "ALL Records DELETED!");
-    t.end();
-  }).end();
-});
+// test("Teardown", function(t) {
+//   drop(function(res){
+//     t.equal(res.acknowledged, true, "ALL Records DELETED!");
+//     t.end();
+//   }).end();
+// });
