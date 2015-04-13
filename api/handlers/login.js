0ce0a0d7d256c4a7c51265bd1c72e52735610045
nelsonic <contact.nelsonic@gmail.com>
2015-04-13 08:34:02 +0100

    adds checker code for no auth. https://github.com/ideaq/time/issues/123

--- api/handlers/login.js
+++ api/handlers/login.js
@@ -1,10 +1,15 @@
 var JWT      = require('../lib/auth_jwt_sign.js');
 var transfer = require('../lib/transfer_anon_to_registered.js'); // see: http://git.io/vvf7k
+var find_all = require('../handlers/timer_find_all'); // see: http://git.io/vvf7k
 var aguid    = require('aguid'); // used to derive the personid
 
 module.exports = function handler(req, reply) {
-    // transfer any records created before login: https://github.com/ideaq/time/issues/102
-    if(!req.payload.email) {
+  // if this method is called without *any* auth header return 401
+  if(!req.headers.authorization && !req.payload.email) { // https://github.com/ideaq/time/issues/123
+    return reply('Please Try Again!').code(401);
+  }
+  else { // // transfer any records created before login
+    if(!req.payload.email) { // see: https://github.com/ideaq/time/issues/102
       req.payload.email = req.auth.credentials.email;
     }
     req.auth = {
@@ -13,4 +18,5 @@ module.exports = function handler(req, reply) {
       }
     }
     return transfer(req, reply);
+  }
 }
