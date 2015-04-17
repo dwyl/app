22c3bf65ab4d14f718c54237d1a8d7793026225a
nelsonic <contact.nelsonic@gmail.com>
2015-04-17 22:02:42 +0100

    implements sending registration confirmation emails with mandrill https://github.com/ideaq/time/issues/85 & https://github.com/ideaq/time/issues/135

--- api/lib/email_welcome.js
+++ api/lib/email_welcome.js
@@ -1,4 +1,3 @@
-var nodemailer = require('nodemailer');
 var fs         = require('fs');
 var path       = require('path');
 var templatedir = __dirname +'/../email_templates/';
@@ -7,30 +6,39 @@ var textpath   = path.resolve(templatedir + 'welcome_text.txt');
 var template   = fs.readFileSync(htmlpath, 'utf8')
 var textonly   = fs.readFileSync(textpath, 'utf8')
 // create reusable transporter object using SMTP transport
-var transporter = nodemailer.createTransport({
-    service: 'Gmail',
-    auth: {
-        user: 'welcome.to.dwyl@gmail.com',
-        pass: process.env.GMAIL_PASSWORD
-    }
-});
+var mandrill    = require('mandrill-api');
+var mandrill_client = new mandrill.Mandrill(process.env.MANDRILL_APIKEY);
 
-// NB! No need to recreate the transporter object. You can use
-// the same transporter object for all e-mails
+module.exports = function email(person, callback) {
+    var message = {
+        "html": template,
+        "text": textonly,
+        "subject": "Welcome to DWYL",
+        "from_email": "hello@dwyl.io",
+        "from_name": "Hello from DWYL",
+        "to": [{
+                "email": person.email,
+                // "name": "FirstName", // not using this for now.
+                "type": "to"
+            }],
+        "headers": {
+            "Reply-To": "hello@dwyl.io"
+        },
+        "important": false,
+        "track_opens": true,
+        "track_clicks": true,
+        "tags": [
+            "registration"
+        ],
+    };
 
-var email = function(person, callback){
-  var mailOptions = {
-    from: '#dwyl do what you love! <welcome.to.dwyl@gmail.com>', // sender
-    to: person.email, // list of receivers
-    subject: 'Welcome to dwyl!', // Subject line
-    text: textonly, // plaintext body
-    html: template
-  };
-  // send mail with defined transport object
-  transporter.sendMail(mailOptions, function(error, info){
-    // console.log(error, info);
-    callback(error, info)
-  });
+    mandrill_client.messages.send({"message": message}, function(result) {
+        console.log(result);
+        return callback(result);
+    }, function(error) {
+        // Mandrill returns the error as an object with name and message keys
+        console.log('MANDRILL ERROR: ' + error.name + ' - ' + error.message);
+        // A mandrill error occurred: Unknown_Subaccount - No subaccount exists with the id 'customer-123'
+    });
+
 }
-
-module.exports = email;
