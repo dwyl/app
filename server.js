4494a15171f66e773971c550d8e2db69118792ed
nelsonic <contact.nelsonic@gmail.com>
2016-07-02 21:34:05 +0100

    a fresh start. with experience. :wink:

--- server.js
+++ server.js
@@ -1,37 +1,23 @@
-// if you want to run the Just the API server without front-end run this file
-require('env2')('.env');
-var Hapi    = require('hapi');
-var Basic   = require('hapi-auth-basic');
-var AuthJWT = require('hapi-auth-jwt2')
-var Joi     = require('joi');
-var lout    = require('lout');
-var ES      = require('esta');  // https://github.com/nelsonic/esta
-var port    = process.env.PORT || 1337; // heroku define port or use 1337
-var server  = new Hapi.Server();
+require('env2')('.env'); // see: https://github.com/dwyl/time/issues/93
+var Hapi   = require('hapi');
+var Joi    = require('joi');
+var server = new Hapi.Server();
 
-server.connection({ port: port });
+server.connection({ port: process.env.PORT });
 
-var routes = require('./routes.js');
-
-server.register([ {register: Basic},
-  {register: AuthJWT}, { register: lout }], function (err) {
-
-  server.auth.strategy('basic', 'basic', {
-    validateFunc: require('./lib/auth_basic_validate.js')
-  });
-
-  // dont force people to set env vars. https://github.com/ideaq/time/issues/93
-  process.env.JWT_SECRET = process.env.JWT_SECRET || 'not-so-secret';
+server.register([
+  {register: require('hapi-auth-jwt2')}], function (err) {
 
   server.auth.strategy('jwt', 'jwt', 'required',  {
     key: process.env.JWT_SECRET,
     validateFunc: require('./lib/auth_jwt_validate.js')
   });
 
-  server.route(routes);
+  server.route(require('./routes.js'));
 });
 
-server.start();
-console.log('Now Visit: http://localhost:'+port);
+server.start(function () {
+  console.log('Now Visit: ' + require('./lib/lanip') + ':' + server.info.port);
+});
 
 module.exports = server;
