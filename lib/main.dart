8464df9d1df29dbc7376cace8adf262bac79364a
Luís Arteiro <lucho14garfield@gmail.com>
2023-02-20 16:40:49 +0000

    feat: Changing icon and app name/description for PWA. #312

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'App',
+        title: 'Dwyl\'s Todo App',
         theme: ThemeData(
           primarySwatch: Colors.teal,
         ),
