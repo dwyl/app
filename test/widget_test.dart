b75199950a2c69b565b4ea339b62a5e7758e17d8
Luís Arteiro <lucho14garfield@gmail.com>
2023-02-20 11:07:09 +0000

    fix: Fixing basic test and adding coverage folder.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,20 +11,10 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:app/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Basic setup, showing the main page', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.text('DWYL'), findsOneWidget);
   });
 }
