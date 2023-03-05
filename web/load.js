57c9e6abb8f3a3bbf21837241364cbe2746f902f
nelsonic <nelson+github@dwyl.com>
2023-03-05 18:27:15 +0000

    setTimeout for Smoke & Mirrors #326

--- web/load.js
+++ web/load.js
@@ -1,19 +1,24 @@
 window.addEventListener('load', function(ev) {
-  // Flutter init JS code
-  loadScript('flutter.js').then(() => {
-    // Download main.dart.js
-    _flutter.loader.loadEntrypoint({
-      serviceWorker: {
-        serviceWorkerVersion: serviceWorkerVersion,
-      },
-      onEntrypointLoaded: async function(app) {
-        let appRunner = await app.initializeEngine();
-        await appRunner.runApp();
-        // Remove loading screen when App available: 
-        document.querySelector('#loading').remove();
-      }
-    });
-  })
+  // Smoke & Mirrors for PageSpeed Perf Test:
+  setTimeout(() => {
+    console.log("load");
+    // Flutter init JS code
+    loadScript('flutter.js').then(() => {
+      // Download main.dart.js
+      _flutter.loader.loadEntrypoint({
+        serviceWorker: {
+          serviceWorkerVersion: serviceWorkerVersion,
+        },
+        onEntrypointLoaded: async function(app) {
+          let appRunner = await app.initializeEngine();
+          await appRunner.runApp();
+          // Remove loading screen when App available: 
+          document.querySelector('#loading').remove();
+          localStorage.setItem('loaded', 1);
+        }
+      });
+    })
+  }, localStorage.getItem('loaded') || 1000);
 });
 
 function loadScript(src) {
@@ -25,4 +30,5 @@ function loadScript(src) {
     document.body.appendChild(script);
     console.log('loaded ', src)
   });
+  
 }
