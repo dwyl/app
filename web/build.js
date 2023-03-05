ece2f857274ec0996fe75a7e01749a71fd11e8e8
nelsonic <nelson+github@dwyl.com>
2023-03-05 08:11:56 +0000

    fix dir traversal in build.js

--- web/build.js
+++ web/build.js
@@ -14,7 +14,7 @@
 // const esbuild = require('esbuild')
 
 require('esbuild').build({
-  entryPoints: ['../build/web/flutter.js','../build/web/main.dart.js'],
+  entryPoints: ['./build/web/flutter.js','./build/web/main.dart.js'],
   bundle: true,
   minify: true,
   allowOverwrite: true,
