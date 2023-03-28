011d890ace91da1aebf29b96ada3da3902e251fd
Nelson <194400+nelsonic@users.noreply.github.com>
2023-03-28 09:51:40 +0100

    Add clarifying comment to each file changed in the patch see: https://github.com/dwyl/app/issues/326#issuecomment-1478314967

--- build_tools/patch_web.sh
+++ build_tools/patch_web.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 
 set -e
-
+# This script is needed to patch the flutter.js and flutter_service_worker.js so they load faster 
+# see: https://github.com/dwyl/app/issues/326#issuecomment-1478314967
 patch ./build/web/flutter.js < ./build_tools/web/flutter.js.patch
 patch ./build/web/flutter_service_worker.js < ./build_tools/web/flutter_service_worker.js.patch
